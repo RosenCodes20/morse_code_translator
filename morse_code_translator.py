a18c4c953982fa9034816e15ff41a3d6535924f8
Rosen Ivanov <143969365+RosenCodes20@users.noreply.github.com>
2023-11-13 20:34:24 +0200

    Create morse_code_translator.py

---
+++ morse_code_translator.py
@@ -0,0 +1,37 @@
+morse_code = input().split(" | ")
+last_result = ""
+letters_dict = {
+    ".-": "A",
+    "-...": "B",
+    "-.-.": "C",
+    "-..": "D",
+    ".": "E",
+    "..-.": "F",
+    "--.": "G",
+    "....": "H",
+    "..": "I",
+    ".---": "J",
+    "-.-": "K",
+    ".-..": "L",
+    "--": "M",
+    "-.": "N",
+    "---": "O",
+    ".--.": "P",
+    "--.-": "Q",
+    ".-.": "R",
+    "...": "S",
+    "-": "T",
+    "..-": "U",
+    "...-": "V",
+    ".--": "W",
+    "-..-": "X",
+    "-.--": "Y",
+    "--..": "Z",
+}
+
+for code in morse_code:
+    for letter in code.split(" "):
+        if letter != "":
+            last_result += letters_dict[letter]
+    last_result += " "
+print(last_result.upper())
